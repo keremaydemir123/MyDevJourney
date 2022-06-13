5d4ad1b97d64bf487ae8fff958bd33a1c72f8479
Kerem Aydemir <keremaydemir59@gmail.com>
2022-06-13 21:56:47 +0300

    gatsby-config changed

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,14 +7,14 @@ module.exports = {
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-minimal-blog/gatsby-config.js
-    siteTitle: `Minimal Blog`,
-    siteTitleAlt: `Minimal Blog - Gatsby Theme`,
-    siteHeadline: `Minimal Blog - Gatsby Theme from @lekoarts`,
-    siteUrl: `https://minimal-blog.lekoarts.de`,
+    siteTitle: `My Dev Journey`,
+    siteTitleAlt: `My Dev Journey`,
+    siteHeadline: `My Dev Journey`,
+    siteUrl: `https://github.com/keremaydemir123`,
     siteDescription: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and line highlighting.`,
     siteLanguage: `en`,
     siteImage: `/banner.jpg`,
-    author: `@lekoarts_de`,
+    author: `kerem aydemir`,
   },
   plugins: [
     {
@@ -34,7 +34,7 @@ module.exports = {
         externalLinks: [
           {
             name: `Twitter`,
-            url: `https://twitter.com/lekoarts_de`,
+            url: `https://twitter.com/KeremAydem1r`,
           },
           {
             name: `GitHub`,
